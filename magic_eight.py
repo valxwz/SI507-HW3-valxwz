116aab125527be23fce4f2aa5c3fc4204b6bbbd1
Valorie Zhang <valoryxiaowen@gmail.com>
2019-09-17 11:50:20 -0400

    Check if it's a question

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,12 @@
-question=input("What's your question?")
+while True:
+	question=input("What's your question? or enter 'quit' to quit \n")
+	if list(question)[-1]!="?" and str(question)!=str("quit"):
+		print ("I'm sorry, I can only answer questions")
+		continue
+		
+	else:
+		print ("Thank you")
+		break
+			
+
+
