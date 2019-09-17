75ae4a556b7a793cda8bd89249fc7b24a357266b
sophie <xqniu@live.unc.edu>
2019-09-17 11:25:05 -0400

    add random answer

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,7 @@
-question=input("What's your question?")
+import random
+
+question=input("What's your question? ")
+
+answer_list = ['It is certain.', 'As I see it, yes.', 'It is decidedly so.','Without a doubt.','Yes - definitely.','You may rely on it.','Most likely.','Outlook good.','Yes.','Signs point to yes.','Reply hazy, try again.','Ask again later.','Better not tell you now.','Cannot predict now.','Concentrate and ask again.','Don not count on it.','My reply is no.','My sources say no.','Outlook not so good.','Very doubtful.']
+
+print(random.choice(answer_list))
